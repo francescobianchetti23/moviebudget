0184326fe9c00dfc7d1ccb9a9ae9fb98153837ff
francescobianchetti23 <francescoricbia@hotmail.com>
2025-01-17 13:34:51 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><meta name="google-adsense-account" content="ca-pub-3940998307424240"></head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
-
